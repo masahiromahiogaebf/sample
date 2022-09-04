ee88c9793e870b8e15e986b2784f3f61766540fc
masahiromahiogaebf <masaizu1029@gmail.com>
2022-09-04 17:23:22 +0900

    basis06

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -4,16 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
-    protected $fillable = [
-    'title',
-    'body',
-　　];
-public function getPaginateByLimit(int $limit_count = 10)
-{
-    // updated_atで降順に並べたあと、limitで件数制限をかける
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-}
-
+class Post extends Model{
+    protected $fillable=[
+        'title',
+        'body',
+    ];
+    
+    public function getPaginateByLimit(int $limit_count = 10){
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
